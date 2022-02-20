9ac0bb2fe1156f25b3656821adae019784a23884
HaiderAli1600 <94439734+HaiderAli1600@users.noreply.github.com>
2022-02-20 16:20:52 +0500

    Add files via upload

---
+++ a.js
@@ -0,0 +1,9 @@
+var loader = document.querySelector(".loader")
+console.log(window.addEventListener("load", vanish));
+function vanish() {
+  loader.classList.add("disppear");
+}// JavaScript Document
+
+
+/*var button=document.getElementsByClassName("selectnone") onclick="setTimeout(function() { alert('Sing-up Successfully! Please Login'); window.location='file:///C:/Users/AliHa/OneDrive/Documents/Desktop/THT%20Web/THT%20Work/pr.html'}, 2500);"
+*/
---
+++ login JA.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+var loader = document.querySelector(".loader")
+console.log(window.addEventListener("load", vanish));
+function vanish() {
+  loader.classList.add("disppear");
+}
